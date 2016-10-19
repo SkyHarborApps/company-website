ead3704aeffa2efeac7e107a1f25b518d3c4e766
Daniel Hollcraft <daniel@hollcraft.com>
2016-10-19 12:52:36 +0200

    adds wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,7 +1,10 @@
 /**
  * Created by danielhollcraft on 10/18/16.
  */
-module.exports = function () {
+var babel = require('babel-core');
+process.env.BABEL_ENV = 'test';
+
+module.exports = function (wallaby) {
 
   return {
     files: [
@@ -11,11 +14,41 @@ module.exports = function () {
 
     tests: ['src/**/*test.js'],
 
-    env: {
-      type: 'node',
-      runner: 'node'
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        babel: babel,
+        presets: ['react-app']
+      })
     },
 
-    testFramework: 'jest'
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
+
+    testFramework: 'jest',
+
+    bootstrap: function (wallaby) {
+      wallaby.testFramework.configure({
+        "moduleFileExtensions": [
+          "jsx",
+          "js",
+          "json"
+        ],
+        "moduleNameMapper": {
+          "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": wallaby.localProjectDir + "config/jest/FileStub.js",
+          "^.+\\.css$": wallaby.localProjectDir + "/config/jest/CSSStub.js",
+          "^.+\\.scss$": wallaby.localProjectDir + "/config/jest/SCSSStub.js"
+        },
+        "setupFiles": [
+          wallaby.localProjectDir + "/config/polyfills.js"
+        ],
+        "testPathIgnorePatterns": [
+          "(build|docs|node_modules)/"
+        ],
+        "testEnvironment": "node",
+        "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(js|jsx)$"
+      });
+    }
   };
 };
