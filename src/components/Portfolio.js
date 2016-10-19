65bbf954cbfd95cf52fc3c5a56eca2cc91c05c7c
Daniel Hollcraft <daniel@hollcraft.com>
2016-10-19 14:23:09 +0200

    small home page refactor. Adds basic template for portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,12 +2,34 @@
  * Created by danielhollcraft on 10/17/16.
  */
 import React from 'react';
+import { Col, Thumbnail, Grid, Row} from 'react-bootstrap';
 
 export const Portfolio = () => {
   return (
-    <p>
-      Portfolio
-    </p>
+    <Row>
+      <Grid>
+        <Row>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+          <Col xs={6} md={3}>
+            <Thumbnail href="#" alt="171x180" src="http://placehold.it/350x350" />
+          </Col>
+        </Row>
+      </Grid>
+    </Row>
   )
 };
 
