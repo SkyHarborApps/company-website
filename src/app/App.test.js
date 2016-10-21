3e01277d3aa6353e3de2d767d5b97978fd017711
Daniel Hollcraft <daniel@hollcraft.com>
2016-10-21 17:10:21 +0200

    Reorganizing file structure as project grows.

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { LinkContainer } from 'react-router-bootstrap'
-import { App } from '../App';
+import { App } from './App';
 import { Grid, Navbar, Jumbotron, NavItem, Nav, Col } from 'react-bootstrap';
 import { browserHistory, Link } from 'react-router'
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './layouts/App';
-import './index.css';
+import { App } from './app/App';
+import '../public/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Home } from './layouts/home/Home'
-import { Blog } from './layouts/blog/Blog'
-import { Portfolio } from './layouts/portfolio/Portfolio'
-import { Contact } from './layouts/contact/Contact'
+import 'font-awesome/css/font-awesome.css'
+import { Home } from './app/home/Home'
+import { Blog } from './app/blog/Blog'
+import { Portfolio } from './app/portfolio/Portfolio'
+import { Contact } from './app/contact/Contact'
 
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
